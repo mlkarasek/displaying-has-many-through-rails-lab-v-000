d0f0d5f691e6808a35d832afc42e8997b790a1ec
Monica  Karasek <mlkarasek@gmail.com>
2018-08-25 06:54:34 +0000

    automatically backed up by learn

--- db/migrate/20180825064242_create_appointments.rb
+++ db/migrate/20180825064242_create_appointments.rb
@@ -3,7 +3,7 @@ class CreateAppointments < ActiveRecord::Migration
     create_table :appointments do |t|
       t.datetime :appointment_datetime
       t.integer :doctor_id
-      t.integer :patient_id 
+      t.integer :patient_id
       
       t.timestamps null: false
     end
